ffffeceb6442da0bb076cb03042175c975a61089
xiaolongtuan <2765504026@qq.com>
2022-04-25 13:07:26 +0800

    添加worker数据表实现数据同步

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ App({
         wx.cloud.init({
           env:'cloud1-5gynw2ctad593524'//开发环境ID
         })
-      }
+      },
+    globalData:{//全局变量
+      user:'',//用户基本信息(users表):昵称和头像链接
+      openID:'',
+      worker:'',//员工的基本信息(worker表)：意向地址，意向工资，datas(投递、交流、收藏的数量)
+    }
 })
--- pages/person/person.js
+++ pages/person/person.js
@@ -1,15 +1,16 @@
 // pages/person/person.js
+const db = wx.cloud.database()
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    user:'',
-    openID:'',
-    person_info:'小龙团',
-    touxiang:'../../images/touxiang.jpg',
-    region: ['广东省', '广州市', '海珠区'],//意向工作地点
+    user:app.globalData.user,
+    openID:app.globalData.openID,
+    worker:app.globalData.worker,
+    region: ['四川省', '广元市', '旺苍县'],//意向工作地点
     multiArray: [['1k', '2k', '3k', '4k', '5k', '6k', '7k', '8k', '9k', '10k', '11k', '12k', '13k', '14k', '15k', '16k', '17k', '18k', '19k'], ['1k', '2k', '3k', '4k', '5k', '6k', '7k', '8k', '9k', '10k', '11k', '12k', '13k', '14k', '15k', '16k', '17k', '18k', '19k', '20k']],
     multiIndex: [0, 0]
   },
@@ -21,6 +22,46 @@ Page({
     this.setData({
       user:wx.getStorageSync('user'),
       openID:wx.getStorageSync('openID')
+    })
+    app.globalData.user = wx.getStorageSync('user')
+    app.globalData.openID = wx.getStorageSync('openID')
+    var that = this
+    db.collection('worker')
+    .where({
+      _openid:this.data.openID
+    })
+    .get({
+      success(res){
+        if(res.data.length >0){
+          that.setData({
+            worker:res.data[0],
+            region:res.data[0].yx_address,
+            multiIndex:res.data[0].yx_salary
+          })
+          app.globalData.worker = res.data[0]
+          console.log("获取成功！",res.data)
+        }
+        else{
+          console.log('添加worker')
+          var worker = {
+            yx_address:['四川省','广元市','旺苍县'],//默认
+            yx_salary:[0,5],
+            datas:[0,0,0]
+          }
+          db.collection('worker')
+          .add({
+            data:{
+              yx_address:['四川省','广元市','旺苍县'],//默认
+              yx_salary:[0,5],
+              datas:[0,0,0]
+            }
+          })
+          app.globalData.worker = worker
+        }
+      },
+      fail(err){
+        console.log("请求失败",err)
+      }
     })
   },
   logup(options) {
@@ -38,15 +79,37 @@ Page({
           success: res2=>{
             console.log("请求云函数成功",res2)
             console.log(res2.result.openid)
-            this.setData({
-              openID:res2.result.openid
-            })
+            // this.setData({
+            //   openID:res2.result.openid
+            // })
+            app.globalData.openID = res2.result.openid
             wx.setStorageSync('openID',res2.result.openid)
-        console.log('缓存openID')
+            console.log('缓存openID')
           }
         })
         wx.setStorageSync('user', res.userInfo)
         console.log('缓存user')
+        wx.cloud.database().collection('users')
+        .add({
+          data:{
+          touxiang: res.userInfo.avatarUrl,
+          name: res.userInfo.nickName
+          }
+        })
+        .then( x=>{
+          console.log('user添加成功',x)
+        })
+        wx.cloud.database().collection('worker')
+        .add({
+          data:{
+          yx_address: this.data.region,
+          yx_salary: this.data.multiIndex,
+          datas:this.data.worker.datas
+          }
+        })
+        .then( x=>{
+          console.log('user添加成功',x)
+        })
       }
     })
   },
@@ -56,12 +119,38 @@ Page({
     this.setData({
       region: e.detail.value
     })
-    this.onLoad()
+    app.globalData.worker.yx_address = e.detail.value
+    this.setData({
+      worker:app.globalData.worker
+    })
+    db.collection('worker')
+    .where({
+      _openid:this.data.openID
+    })
+    .update({
+      data:{
+        yx_address:e.detail.value
+      }
+    })
+    // this.onLoad()
   },
   bindMultiPickerChange: function (e) {
     //console.log('picker发送选择改变,携带值为', e.detail.value)
     this.setData({
       multiIndex: e.detail.value
+    })
+    app.globalData.worker.yx_salary = e.detail.value
+    this.setData({
+      worker:app.globalData.worker
+    })
+    db.collection('worker')
+    .where({
+      _openid:this.data.openID
+    })
+    .update({
+      data:{
+        yx_salary:e.detail.value
+      }
     })
   },
   bindMultiPickerColumnChange: function(e){//第二位数字始终大于第一位
