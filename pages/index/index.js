af85415db1c53ea09125a5cfbdc4f4f10a80de75
bayunqi <1677922504@qq.com>
2022-05-08 22:22:38 +0800

    byq 提交 index--首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,2 +1,26 @@
 // index.js
-Page({})
+Page({
+  data: {
+    //滑动幻灯片素材
+    swiperImg: [
+      {src: 'https://img-qn-4.51miz.com/preview/muban/00/00/78/71/M-787115-34711ABE.jpg!/quality/90/unsharp/true/compress/true/fh/400'},
+      {src: 'https://img-qn-3.51miz.com/preview/element/00/01/20/65/E-1206525-53D99744.jpg!/quality/90/unsharp/true/compress/true/format/jpg/fh/320'},
+      {src: 'https://img-qn-0.51miz.com/preview/muban/00/00/67/86/M-678623-5C6790F7.jpg!/quality/90/unsharp/true/compress/true/fh/400'}
+    ],
+    //临时招聘信息
+    jobsList:[{
+      id: '264698',
+      title: '北京邮电大学家里蹲食堂服务员',
+      poster: 'https://p0.ssl.qhimgs1.com/sdr/400__/t01905178e193e4a73e.jpg',
+      salary: '-1k',
+      address: '北京邮电大学家里蹲分校',
+      pnum: '0-999人'
+    }]
+  },
+  goToDetail: function(e) {
+    let id=e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: '../detail/detail?id='+id
+    })
+  }
+})
