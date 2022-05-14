b42b6fbd8fc433b1ddaaa3d58cdd63e5043f13ca
bayunqi <1677922504@qq.com>
2022-05-14 16:42:30 +0800

    5.14 - 正在完成已投递简历展示和已收到简历展示部分

---
+++ pages/worker_resume/worker_resume.js
@@ -0,0 +1,110 @@
+// pages/worker_resume/worker_resume.js
+const db = wx.cloud.database()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isboss: false,
+    worker_resume: [],
+    jobs: {},
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    this.setData({isboss:wx.getStorageSync('isboss')})
+    var that = this  
+    db.collection('worker')
+      .where({
+        _openid: this.data.openID
+      })
+      .get({
+        success(res) {
+          if(res.data.length >0){
+            var worker_sended = that.res.data[0].worker_sended
+            if(!that.data.isboss){
+              that.setData({worker_resume: worker_sended})
+            }
+
+          }
+        }
+      })
+
+      db.collection('jobs').get()
+      .then(res => {
+        console.log(res.data)
+        let result = that.getjobs(that.data.worker_resume, res.data)
+        that.setData({jobs:result})
+        console.log(that.data.worker_resume)
+        console.log(that.data.jobs)
+      })
+      .catch(err => {
+        console.log('请求失败',err)
+      })  
+  },
+
+  getjobs: function(jobsId, jobsList) {
+    var len = jobsId.length;
+    var size = jobsList.length;
+    let jobs = [];
+    for(let i = 0; i < len; i++) {
+      for(let j = 0; j < size; j++) {
+        if(jobsId[i] == jobsList[j]._id) {
+          jobs.push(jobsList[j])
+        }
+      }
+    }
+    return jobs;
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
