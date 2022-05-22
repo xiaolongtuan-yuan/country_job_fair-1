27c03abed682e638c6e29d807f5b41a8522d45ee
xiaolongtuan <2765504026@qq.com>
2022-05-22 15:40:49 +0800

    首页美化

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,16 +7,10 @@ Page({
       {src: 'https://img.zcool.cn/community/0128cf56efbc5732f875a944e947a6.jpg?x-oss-process=image/auto-orient,1'},
       {src: 'https://img-qn-0.51miz.com/preview/muban/00/00/67/86/M-678623-5C6790F7.jpg!/quality/90/unsharp/true/compress/true/fh/400'}
     ],
-    //临时招聘信息
-    // jobsList:[{
-    //   id: '264698',
-    //   title: '北京邮电大学家里蹲食堂服务员',
-    //   poster: 'https://p0.ssl.qhimgs1.com/sdr/400__/t01905178e193e4a73e.jpg',
-    //   salary: '-1k',
-    //   address: '北京邮电大学家里蹲分校',
-    //   pnum: '0-999人'
-    // }]
     jobsList: [{
+
+    }],
+    jobsList2:[{
 
     }]
   },
@@ -26,9 +20,13 @@ Page({
   onLoad: function (options) {
     wx.cloud.database().collection('jobs').get()
       .then(res => {
+        var len = res.data.length
+        var half = Math.round(len/2)
         this.setData({
-          jobsList: res.data
+          jobsList: res.data.slice(0,half),
+          jobsList2: res.data.slice(half,len)
         })
+        console.log(res.data)
       })
       .catch(err => {
         console.log('请求失败',err)
