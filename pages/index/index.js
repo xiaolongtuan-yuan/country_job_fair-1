1d82630dfee780ff1394fe2c011bc53862049101
xiaolongtuan <2765504026@qq.com>
2022-06-02 10:04:32 +0800

    首页动画，删除了一些二图片

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,13 +12,20 @@ Page({
     }],
     jobsList2:[{
 
-    }]
+    }],
+    mode:true //true是展示招聘信息,false时展示简历
   },
    /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    wx.cloud.database().collection('jobs').get()
+    this.app = getApp()
+    console.log('当前用户地区',this.app.globalData.worker.yx_address)
+    wx.cloud.database().collection('jobs')
+      .where({
+        region:this.app.globalData.worker.yx_address
+      })
+      .get()
       .then(res => {
         var len = res.data.length
         var half = Math.round(len/2)
@@ -37,5 +44,41 @@ Page({
     wx.navigateTo({
       url: '../detail/detail?id='+id
     })
+  },
+  changemode(e){
+    console.log("改变模式",e.currentTarget.dataset.mode)
+    var mode = e.currentTarget.dataset.mode
+    if(mode=='boss'){
+      this.setData({
+        mode:true
+      })
+      this.app.sliderightshow(this, 'slide_up1', 0)
+    }
+    else{
+      this.setData({
+        mode:false
+      })
+      this.app.sliderightshow(this, 'slide_up1', -750)
+    }
+  },
+  onShow:function(){
+    console.log('当前用户地区',this.app.globalData.worker.yx_address)
+    wx.cloud.database().collection('jobs')
+      .where({
+        region:this.app.globalData.worker.yx_address
+      })
+      .get()
+      .then(res => {
+        var len = res.data.length
+        var half = Math.round(len/2)
+        this.setData({
+          jobsList: res.data.slice(0,half),
+          jobsList2: res.data.slice(half,len)
+        })
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log('请求失败',err)
+      })
   }
 })
