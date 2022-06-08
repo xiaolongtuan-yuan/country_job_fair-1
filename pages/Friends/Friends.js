0b264066b208dae92ba338a87e3d4e59c9a1b217
xiaolongtuan <2765504026@qq.com>
2022-06-08 13:42:38 +0800

    退出登录清理friend数据

--- pages/Friends/Friends.js
+++ pages/Friends/Friends.js
@@ -15,7 +15,7 @@ Page({
     date:[],
     unread:[],
     id:"",
-    firstload:true
+    firstload:true,
   },
 
   /**
@@ -51,6 +51,15 @@ Page({
   onShow() {
     //登录判断
     if (app.globalData.openID.length === 0) {
+      this.setData({
+        Friends:[],
+        briefMsg:[],
+        FriendsUserInfo:[],
+        date:[],
+        unread:[],
+        id:"",
+        firstload:true
+      })
       wx.showToast({
         title: '您未登录~',
         image: '/images/icons/error.png'
